af7098a833940c101796d9adce48bc28d045f8ba
Nikita Denisov <dennikita03@gmail.com>
2022-01-06 22:12:18 +0300

    Add classe of Cell and first generation processing

--- package/GameMap.py
+++ package/GameMap.py
@@ -1,2 +1,12 @@
+import GameCell
+
+
 class GameMap:
-    pass
+
+    __height, __width = 0, 0
+    map = []
+
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+        self.map = [[GameCell.GameCell() for _i in range(width)] for _j in range(height)]
